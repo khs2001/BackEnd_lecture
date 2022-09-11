0012b58a29302805208fc35bfa205f2103200a9a
2021011121 <khs2776413678@gmail.com>
2022-09-11 22:17:01 +0900

    first

---
+++ app.js
@@ -0,0 +1,38 @@
+// const http=require("http");
+// const app=http.createServer((req,res)=>{
+//     res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
+//     if(req.url=="/"){
+//         res.end("여기는 루트 화면입니다.");
+//     }
+//     else if(req.url=="/login"){
+//         res.end("여기는 로그인 화면입니다.");
+//     }
+// });
+
+// app.listen(3001,()=>{
+//     console.log("http로 가동된 서버입니다.");
+// });
+"use strict";
+
+//모듈
+const express =require("express");
+const bodyParser=require("body-parser");
+const app=express();
+
+const POST=3000;
+
+//라우팅
+const home=require("./src/routes/home");
+
+//앱 세팅 
+app.set("views","./src/views");
+app.set("view engine","ejs");
+app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+//URL을 통해 전달되는 데이터에 한글,공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use("/",home);
+
+module.exports=app;
---
+++ src/public/js/home/login.js
@@ -0,0 +1,37 @@
+"use strict"
+
+// const  {JSON } = require("body-parser");
+const id=document.querySelector("#id"),
+psword=document.querySelector("#psword"),
+loginBtn=document.querySelector("#button")
+
+loginBtn.addEventListener("click",login);
+
+function login(){
+    const req={ 
+        id:id.value,
+        psword:psword.value,
+    };
+    // console.log(req);
+    // console.log(req,JSON.stringify(req));
+    fetch("/login",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify(req),
+    })
+    .then((res)=>res.json())
+    .then((res)=>{
+        if(res.success){
+            location.href="/";
+        }
+        else{
+            alert(res.msg);
+        }
+    })
+    .catch((err)=>{
+        console.error("로그인 중 에러 발생");
+    }); 
+    
+}
